08062e46b01ceb611180997413c0dc288be33b99
Leo <u6520057@au.edu>
2024-08-24 23:35:31 +0700

    Adjust table and chart to show side by side

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,52 +1,58 @@
 import React from "react";
 import { useState } from "react";
-import {Col, Row, Container, Button} from 'react-bootstrap';
+import { Col, Row, Container, Button } from 'react-bootstrap';
 import DataTable from "../Components/DataTable";
 import PieChart from "../Components/piechart";
 import carList from "../assets/taladrod-cars.json";
 import StackedBarChart from "../Components/StackedbarChart2";
 
 const DashboardPage = () => {
-    const {Cars} = carList
-    const {MMList} = carList
-    const [page, setPage] = useState("table")
+  const { Cars } = carList
+  const { MMList } = carList
+  const [page, setPage] = useState("table")
 
-    const handleTableChange = () => {
-        if (page == "table") {
-            setPage("detail_table")
-        }else {
-            setPage("table")
-        }
+  const handleTableChange = () => {
+    if (page == "table") {
+      setPage("detail_table")
+    } else {
+      setPage("table")
     }
-    return (
-        <Container>
-        <h1>Dashboard</h1>
+  }
+  return (
+    <Container>
+      <h1>Dashboard</h1>
+      <Row>
         <Row>
-        <Col xs={3}>
-        <Button  onClick={handleTableChange}>{page == "table" ? "View Detail" : "View Less"}</Button>
-        </Col>
+          <Col xs={3}>
+            <Button onClick={handleTableChange}>{page == "table" ? "View Detail" : "View Less"}</Button>
+          </Col>
+        </Row>
         <br />
 
-        {page == "table" &&
-        <Row>
-          <div style={{ height: '500px', marginBottom: '20px' }}>
-            <PieChart data={Cars} />
-          </div>
-        </Row>
-        }
+        <Col md={5}>
+          <DataTable data={Cars} brand={MMList} name={page} />
+        </Col>
 
-        {page == "detail_table" &&
-        <Row>
-        <div style={{ height: '1000px',  width: '100%', overflowX: 'auto' }}>
-          <StackedBarChart data={Cars} />
-        </div>
-      </Row>
-        }
+        <Col md={7}>
+          {page == "table" &&
+            <Row>
+              <div style={{ height: '500px', marginBottom: '20px' }}>
+                <PieChart data={Cars} />
+              </div>
+            </Row>
+          }
 
-<DataTable data={Cars} brand={MMList} name={page} />
+          {page == "detail_table" &&
+            <Row>
+              <div style={{ height: '1000px', width: '100%', overflowX: 'auto' }}>
+                <StackedBarChart data={Cars} />
+              </div>
+            </Row>
+          }
+        </Col>
       </Row>
-      </Container>
-    );
+    </Container>
+  );
 };
 
 export default DashboardPage;
